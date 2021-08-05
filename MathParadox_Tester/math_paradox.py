48006c7de1c384dd98d6c047b3ed64424c5e265b
Kris P <krisnpenn827@gmail.com>
2021-08-04 23:57:00 -0500

    Added comments to math_paradox.py further explaining the steps.

--- MathParadox_Tester/math_paradox.py
+++ MathParadox_Tester/math_paradox.py
@@ -1,24 +1,24 @@
 def main():
 
-    start = int(input("Please enter any positive integer: "))
-    path = start % 2
+    start = int(input("Please enter any positive integer: "))   #initial value, assuming only a positive integer is added.
+    path = start % 2    # prevents recursion overflow
 
-    if start == 1  or start == 2 or start == 4:
+    if start == 1  or start == 2 or start == 4: # checks if it hits paradox loop.
         print("\nThis number ends in a paradox.")
         return
-    elif path == 0:
+    elif path == 0: #if number is even do the even math.
         even(start)
         return
-    elif path == 1:
+    elif path == 1: #if number is odd do the odd math.
         odd(start)
         return
-    else:
+    else:           #catchall in-case something goes wrong. (theoretically shouldn't happen.)
         print("Something went wrong. Restart and try again.")
         return    
 
 def even(start):
     start /= 2
-    path = start % 2
+    path = start % 2    # prevents recursion overflow
 
     if start == 1 or start == 2 or start == 4:
         print("\nThis number ends in a paradox.")
@@ -36,7 +36,7 @@ def even(start):
 
 def odd(start):
     start = start * 3 + 1
-    path = start % 2
+    path = start % 2    # prevents recursion overflow
 
     if start == 1  or start == 2 or start == 4:
         print("\nThis number ends in a paradox.")
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # RandomProjects
 Assorted projects I do in my free time.
 
+FlappyBird:
+  I created this to help me learn Java.  I mostly followed a tutorial as it was my first attempt at using jframes and graphical interfaces.  I also used this project to learn how to
+  compile a .jar of the program to execute without running through IntelliJ, although you do need java to make it work.  Overall its a pretty basic application but I learned a lot from it.
+
 MathParadox_Tester:
   MathParadox_Tester is a single file application I developed using python.  Its purpose is to test the paradoxical, mathematical problem where ANY integer will end in a 4-2-1 loop
   given the following rules:  If the number is even, divide by 2, and if the number is odd, multiply by 3 and add 1 to the product.  This is, by nature, a recursive function so that
